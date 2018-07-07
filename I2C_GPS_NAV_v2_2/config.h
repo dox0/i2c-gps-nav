3c2300627ecc809e9c04e7af0411a7c88116ea24
dox0 <dox0@qq.com>
2018-07-08 00:08:55 +0800

    1234

--- I2C_GPS_NAV_v2_2/config.h
+++ I2C_GPS_NAV_v2_2/config.h
@@ -1,17 +1,17 @@
 //////////////////////////////////////////////////////////////////////////////
 // i2C comm definitions
 //
-#define I2C_ADDRESS        0x20                      //7 bit address 0x40 write, 0x41 read
+#define I2C_ADDRESS         0x20                      //7 bit address 0x40 write, 0x41 read
 
 /* GPS Lead filter - predicts gps position based on the x/y speed. helps overcome the gps lag. */
 #define GPS_LEAD_FILTER
 
 /* Serial speed of the GPS */
-#define GPS_SERIAL_SPEED 38400
+#define GPS_SERIAL_SPEED    38400
 
 /* GPS protocol 
- * NMEA			- Standard NMEA protocol GGA, GSA and RMC  sentences are needed
- * UBLOX		- U-Blox binary protocol, use the ublox config file (u-blox-config.ublox.txt) from the source tree 
+ * NMEA         - Standard NMEA protocol GGA, GSA and RMC  sentences are needed
+ * UBLOX        - U-Blox binary protocol, use the ublox config file (u-blox-config.ublox.txt) from the source tree 
  * MTK_BINARY16 - MTK binary protocol (DIYDrones v1.6) 
  * MTK_BINARY19 - MTK binary protocol (DIYDrones v1.9) 
  * MTK_INIT     - Initialize MTK GPS (if MTK_BINARY16 or 19 is not defined then it goes to NMEA, otherwise it goes for binary)
@@ -33,11 +33,11 @@
 // Ping/Pong type sonar modules such as HC-SR04, SRF-04, DYP-ME007 and many others...
 // PWM output continous sonars such as the MAXBOTIX series (connect pin2(PWM out) of the sonar to PC2
 
-#define SONAR						// USE Sonar
+#define SONAR                       // USE Sonar
 
 //Sonar type uncomment only one at a time
 //#define PINGPONG
-#define MAXBOTIX_PWM				// PWM output mode sonar
+#define MAXBOTIX_PWM                // PWM output mode sonar
 
 
 
@@ -45,29 +45,29 @@
 //////////////////////////////////////////////////////////////////////////////
 // POSHOLD control gains
 //
-#define POSHOLD_P		.11
-#define POSHOLD_I		0.0
-#define POSHOLD_IMAX		20		        // degrees
+#define POSHOLD_P           .11
+#define POSHOLD_I           0.0
+#define POSHOLD_IMAX        20          // degrees
 
-#define POSHOLD_RATE_P		1.4			//
-#define POSHOLD_RATE_I		0.2			// Wind control
-#define POSHOLD_RATE_D		0.010			// try 2 or 3 for POSHOLD_RATE 1
-#define POSHOLD_RATE_IMAX	20			// degrees
+#define POSHOLD_RATE_P      1.4         //
+#define POSHOLD_RATE_I      0.2         // Wind control
+#define POSHOLD_RATE_D      0.010       // try 2 or 3 for POSHOLD_RATE 1
+#define POSHOLD_RATE_IMAX   20          // degrees
 //////////////////////////////////////////////////////////////////////////////
 // Navigation PID gains
 //
-#define NAV_P			1.4	        	//
-#define NAV_I			0.20		        // Wind control
-#define NAV_D			0.006		        //
-#define NAV_IMAX		20		        // degrees
+#define NAV_P               1.4         //
+#define NAV_I               0.20        // Wind control
+#define NAV_D               0.006       //
+#define NAV_IMAX            20          // degrees
 
 ////////////////////////////////////////////////////////////////////////////////////
 // Navigational parameters and limiters initial values
 //
-#define CROSSTRACK_GAIN            1            // Weighting the cross track error
-#define NAV_SPEED_MIN              100          // cm/sec minimum navigational speed when NAV_SLOW_NAV id false
-#define NAV_SPEED_MAX              300          // cm/sec maximum navigational speed
-#define NAV_BANK_MAX               2500         // 20deg max banking when navigating (just for security and testing)
+#define CROSSTRACK_GAIN     1            // Weighting the cross track error
+#define NAV_SPEED_MIN       100          // cm/sec minimum navigational speed when NAV_SLOW_NAV id false
+#define NAV_SPEED_MAX       300          // cm/sec maximum navigational speed
+#define NAV_BANK_MAX        2500         // 20deg max banking when navigating (just for security and testing)
 
 ////////////////////////////////////////////////////////////////////////////////////
 // GPS data filtering - moving average filter vector length
